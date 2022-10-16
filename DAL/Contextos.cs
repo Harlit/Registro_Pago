9fb2bf03266bcfa7286a21e9a800666e3b95fca6
Harlit <harlit.estevez@gmail.com>
2022-10-16 16:12:17 -0400

    se agrega la rama rPrestamos

--- DAL/Contextos.cs
+++ DAL/Contextos.cs
@@ -5,6 +5,8 @@ public class Contexto : DbContext
     public DbSet<Pagos> Pagos { get; set; }
     public DbSet<Ocupaciones> Ocupaciones { get; set; }
     public DbSet<Personas> Personas { get; set; }
+
+    public DbSet<Prestamos> Prestamos { get; set; }
     public Contexto(DbContextOptions<Contexto> options) : base(options)
     {
     }
