7ea8557e1640208861f2be0d29fc6aef7f957383
Harlit <harlit.estevez@gmail.com>
2022-10-16 13:45:11 -0400

    se agrega models persona y contexto

--- DAL/Contextos.cs
+++ DAL/Contextos.cs
@@ -4,6 +4,7 @@ public class Contexto : DbContext
 {
     public DbSet<Pagos> Pagos { get; set; }
     public DbSet<Ocupaciones> Ocupaciones { get; set; }
+    public DbSet<Personas> Personas { get; set; }
     public Contexto(DbContextOptions<Contexto> options) : base(options)
     {
     }
