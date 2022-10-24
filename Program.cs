411fd062cb494509ec8781a25a6c9e2b3f295812
Harlit <harlit.estevez@gmail.com>
2022-10-24 15:00:29 -0400

    log in (tiene errores por resolver)

--- Program.cs
+++ Program.cs
@@ -1,32 +1,34 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
-using Radzen;
-
+using RegistroPago.Areas.Identity;
+using RegistroPago.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var ConStr = builder.Configuration.GetConnectionString("ConStr");
-
-
-
-builder.Services.AddDbContext<Contexto>(options =>
-options.UseSqlite(ConStr)
-);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<PagosBLL>();
-builder.Services.AddScoped<OcupacionesBLL>();
-builder.Services.AddScoped<PersonasBLL>();
-builder.Services.AddScoped<PrestamosBLL>();
-builder.Services.AddScoped<NotificationService>();
-
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -39,6 +41,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
